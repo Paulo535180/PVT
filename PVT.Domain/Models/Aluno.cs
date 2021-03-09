8f33923566762d2c636dfb11e5da7cf4dfefe4b1
paulo535180 <paulo535180@gmail.com>
2021-03-09 16:52:37 -0300

    criar login aluno.

--- PVT.Domain/Models/Aluno.cs
+++ PVT.Domain/Models/Aluno.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace PVT.Domain.Models
 {
-    public class Aluno
+    [Table("PVT_ALUNO")]
+    public class Aluno : Entity
     {
-
+        [Required]
+        public string NOME { get; set; }
+        public string EMAIL { get; set; }
+        public string SENHA { get; set; }
     }
 }
---
+++ PVT.Domain/Models/Matricula.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PVT.Domain.Models
+{
+    [Table("PVT_MATRICULA")]
+    public class Matricula : Entity
+    {
+        public int ID_ALUNO { get; set; }
+        public int ID_SETOR_MODULO { get; set; }
+    }
+}
